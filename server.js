2cef16b1ba278ed9e1a851016e08786e9ccdce2e
unknown <brian_warner25@yahoo.com>
2018-07-14 13:01:00 -0700

    post

--- server.js
+++ server.js
@@ -6,26 +6,26 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const tables = [
-  {
-    tableNo: 1,
-    status: true
-  },
-  {
-    tableNo: 2,
-    status: false
-  },
-  {
-    tableNo: 3,
-    status: false
-  },
-  {
-    tableNo: 4,
-    status: false
-  },
-  {
-    tableNo: 5,
-    status: false
-  }
+	{
+		tableNo: 1,
+		status: false
+	},
+	{
+		tableNo: 2,
+		status: false
+	},
+	{
+		tableNo: 3,
+		status: false
+	},
+	{
+		tableNo: 4,
+		status: false
+	},
+	{
+		tableNo: 5,
+		status: false
+	}
 ];
 
 const reservations = [];
@@ -37,75 +37,67 @@ app.use(parser.json());
 /* Start Workspace */
 
 // New Reservation Handling
-app.post("/api/new", function(req, res){
+app.post("/api/new", function (req, res) {
 	var newTable = req.body;
 
-    var doWeNeedToDoSomethingElse = true;
-    for(i = 0; i < tables.length; i++){
-        if (tables.status === false){
-			reservations.push(res.json(newTable));
-            table.status = true
-            doWeNeedToDoSomethingElse = false;
-            break;
-        }
-    }
+	var control = true;
+	for (i = 0; i < tables.length; i++) {
+		if (tables[i].status === false) {
 
-    if (doWeNeedToDoSomethingElse) {
-		waitingList.push(res.json(newTable))
-		console.log("You have been added to the waiting list!")
-		doWeNeedToDoSomethingElse = true;
-}
-
-    
-
-    res.json({message: "Added Reservation"})
-    res.json({message: "Sorry but you have been added to the waiting list"})
-    res.json(newTable);
-
+			reservations.push(newTable);
+			tables[i].status = true;
+			control = false;
+			res.json({ message: "Added Reservation", data: tables });
+			break;
+		}
+	}
+	if (control) {
+		waitingList.push(newTable)
+		res.json({ message: "Sorry but you have been added to the waiting list", data: tables })
+	}
 });
 
 // Retrive Reservation JSON
-app.get("/api/reservations", function(req, res) {
-	
-	if (reservations.length===0)
-	{
+app.get("/api/reservations", function (req, res) {
+
+	if (reservations.length === 0) {
 		return res.json([]);
 	}
 	else {
 
-	return res.json({
-		 reservations:  reservations,
-	     waiting :  waitingList 
-	})
+		return res.json({
+			reservations: reservations,
+			waiting: waitingList
+		})
 
-}
+	}
 
 })
 
 // Serve HTML pages
-app.get(/\/(([\w]+)\.html)$/, function(req, res, next) {
-  const fileName = req.params[0];
-  res.sendFile(
-    fileName,
-    {
-      root: __dirname + "/",
-      dotfiles: "deny",
-      headers: {
-        "x-timestamp": Date.now(),
-        "x-sent": true
-      }
-    },
-    function(err) {
-      if (err) {
-        next(err);
-      } else {
-        console.log("Sent:", fileName);
-      }
-    }
-  );
+app.get(/\/(([\w]+)\.html)$/, function (req, res, next) {
+	const fileName = req.params[0];
+	res.sendFile(
+		fileName,
+		{
+			root: __dirname + "/",
+			dotfiles: "deny",
+			headers: {
+				"x-timestamp": Date.now(),
+				"x-sent": true
+			}
+		},
+		function (err) {
+			if (err) {
+				next(err);
+			} else {
+				console.log("Sent:", fileName);
+			}
+		}
+	);
 });
 
 /* End Workspace */
-app.listen(PORT, function() {
-  console.log(`http://localhost:${PORT}`);
+app.listen(PORT, function () {
+	console.log(`http://localhost:${PORT}`);
 });
